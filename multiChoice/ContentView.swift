b0923895d461e66a2e4c3e617b4647ba482f188d
jovytta <jovyttayauwanta@gmail.com>
2025-04-25 10:28:37 +0800

    Initial Commit

---
+++ multiChoice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  multiChoice
+//
+//  Created by Mac12 on 2025/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ multiChoice/multiChoiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  multiChoiceApp.swift
+//  multiChoice
+//
+//  Created by Mac12 on 2025/4/25.
+//
+
+import SwiftUI
+
+@main
+struct multiChoiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
